1b32af43b43082ae20146a7ea221a4b98d641826
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-23 22:39:47 +0530

    Add files via upload

---
+++ Exercise-4.py
@@ -0,0 +1,19 @@
+#Exercise-4: Pattern Printing
+""" Question: Input = int
+Boolean = True or False
+If True Print Pattern like this:
+*
+* *
+* * *
+* * * *
+
+If False Print Pattern like this:
+* * * *
+* * *
+* *
+*
+"""
+
+one= print(int(input("Enter the number of rows: ")))
+two = print(int(input("Write 1 or 0: ")))
+new = bool(two)

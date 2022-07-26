6644777191204dd77c070939390e2a81311cf0de
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-26 22:35:21 +0530

    Add files via upload

---
+++ Exercise-5.py
@@ -0,0 +1,12 @@
+# Exercise-5 : Health Management System
+""" use this function:
+                      def getdata():
+                          import datetime
+                          return datetime.datetime.now()
+"""
+""" 3 Clients - Siddhesh, Ananya, John 
+Prepare 3 files for locking their food.
+also prepare another 3 files for locking their exercise.
+Now write a function that when executed takes as input as-
+(i) Client's name.
+(ii) """
